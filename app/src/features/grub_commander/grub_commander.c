823ca825c0ede4826b8e6454990ffd7b97fea386
Helmut Lord <kellyhlord@gmail.com>
2024-04-06 23:54:13 -0400

    features: grub_commander: added ipc, read previous
    grub commander now communicates with its tab
    and is careful to read the previous setting
    it had in NVM
    
    Signed-off-by: Helmut Lord <kellyhlord@gmail.com>

--- app/src/features/grub_commander/grub_commander.c
+++ app/src/features/grub_commander/grub_commander.c
@@ -5,9 +5,19 @@
 #include <zephyr/fs/fs.h>
 #include <ff.h>
 
+#include "zippy/zippy_msg.h"
+
 #include "features/grub_commander/grub_commander.h"
 
 LOG_MODULE_REGISTER(grub_commander);
+
+ZBUS_CHAN_DEFINE(grub_commander_chan,
+                 struct gc_message,
+                 NULL,
+                 NULL,
+                 ZBUS_OBSERVERS(grub_commander_tab, grub_commander_feature),
+                 ZBUS_MSG_INIT(0)
+);
 
 #define GRUB_CFG_PATH "/NAND:/boot_opt.cfg"
 
@@ -34,13 +44,43 @@ static int grub_write_boot_opt(int index) {
     return err;
 }
 
+int grub_get_boot_opt(uint16_t *index) {
+    struct fs_file_t file;
+    fs_file_t_init(&file);
+
+    int err = fs_open(&file, GRUB_CFG_PATH, FS_O_READ);
+    if (err) {
+        index = 0;
+        return err;
+    }
+
+    char buf[32];
+    fs_read(&file, buf, sizeof(buf));
+    fs_close(&file);
+
+    for (int i = 0; i < OS_HW_SWITCH_OPTIONS; i++) {
+        if (strncmp(buf, os_hw_switch_options[i], strlen(os_hw_switch_options[i])) == 0) {
+            *index = i;
+            return 0;
+        }
+    }
+
+    return -EINVAL;
+}
+
 static int grub_commander_init(void) {
     int err;
 
-    err = grub_write_boot_opt(0);
+    uint16_t index;
+    err = grub_get_boot_opt(&index);
     if (err) {
-        LOG_ERR("Failed to write boot option: %d", err);
-        return err;
+        LOG_ERR("Failed to get boot option: %d", err);
+
+        err = grub_write_boot_opt(index);
+        if (err) {
+            LOG_ERR("Failed to write boot option: %d", err);
+            return err;
+        }
     }
 
     return 0;
@@ -48,21 +88,23 @@ static int grub_commander_init(void) {
 
 SYS_INIT(grub_commander_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
 
-static void zbus_cb_grub_commander(const struct zbus_channel *chan) {
+static void zbus_cb_grub_commander_feature(const struct zbus_channel *chan) {
     int err;
 
-    const struct gc_index_message *selected_option = zbus_chan_const_msg(chan);
+    const struct gc_message *selected_option = zbus_chan_const_msg(chan);
 
-    uint16_t index = selected_option->index;
+    if (selected_option->type == MSG_TYPE_UI) {
+        uint16_t index = selected_option->index;
 
-    err = grub_write_boot_opt(index);
-    if (err) {
-        LOG_ERR("Failed to write boot option: %d", err);
-    }
-    else {
-        LOG_INF("Selected option: %d", selected_option->index);
+        err = grub_write_boot_opt(index);
+        if (err) {
+            LOG_ERR("Failed to write boot option: %d", err);
+        }
+        else {
+            LOG_INF("FEATURE: Selected option: %d", selected_option->index);
+        }
     }
 }
 
-ZBUS_LISTENER_DEFINE(grub_commander_listener, zbus_cb_grub_commander);
+ZBUS_LISTENER_DEFINE(grub_commander_feature, zbus_cb_grub_commander_feature);
 
